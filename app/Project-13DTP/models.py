3c3f6ab6396fb3ddf853688dcc1399fde8b5af42
IsorokuYamamot0 <21437@burnside.school.nz>
2025-05-21 19:27:07 +1200

    Changing the layout and adding a search html. Also testing to see if my sql alchemy works

--- app/Project-13DTP/models.py
+++ app/Project-13DTP/models.py
@@ -1,84 +1,38 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
-class MilitaryData(db.Model):
-    __tablename__ = 'militarydata'
-    VehicleID = db.Column(db.Integer, primary_key=True)
-    Country = db.Column(db.String)
+class Plane(db.Model):
+    __tablename__ = 'Planes'
+    id_planes = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String)
+
+class Tank(db.Model):
+    __tablename__ = 'Tanks'
+    id_Tanks = db.Column(db.Integer, primary_key=True)
+    Tank_Name = db.Column(db.String, nullable=False)
 
 class ICBM(db.Model):
-    __tablename__ = 'icbm'
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
+    __tablename__ = 'ICBM'
+    id_ICBM = db.Column(db.Integer, primary_key=True)
+    ICBM_Name = db.Column(db.String)
 
-class Tanks(db.Model):
-    __tablename__ = 'tanks'
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
+class Helicopter(db.Model):
+    __tablename__ = 'Helicopter'
+    id_Heli = db.Column(db.Integer, primary_key=True)
+    Heli_Name = db.Column(db.String)
 
-class Helicopters(db.Model):
-    __tablename__ = 'helicopters'
+class AirGroundMissile(db.Model):
+    __tablename__ = 'AirGroundMissiles'
     ID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
 
-class Planes(db.Model):
-    __tablename__ = 'planes'
+class SurfaceMissile(db.Model):
+    __tablename__ = 'SurfaceMissiles'
     ID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
 
-class AirGroundMissiles(db.Model):
-    __tablename__ = 'airgroundmissiles'
+class SeaSurfaceMissile(db.Model):
+    __tablename__ = 'SeaSurfaceMissiles'
     ID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
-
-class SurfaceMissiles(db.Model):
-    __tablename__ = 'surfacemissiles'
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
-
-class SeaSurfaceMissiles(db.Model):
-    __tablename__ = 'seasurfacemissiles'
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
-
-class Ships(db.Model):
-    __tablename__ = 'ships'
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
-    Type = db.Column(db.String)
-
-class Submarines(db.Model):
-    __tablename__ = 'submarines'
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
-    Class = db.Column(db.String)
-
-class Drones(db.Model):
-    __tablename__ = 'drones'
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
-    Type = db.Column(db.String)
-
-class MissileDefenseSystems(db.Model):
-    __tablename__ = 'missiledefensesystems'
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
-    Range = db.Column(db.String)
-
-class NavalAircraft(db.Model):
-    __tablename__ = 'navalaircraft'
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String)
-    CountryID = db.Column(db.Integer, db.ForeignKey('militarydata.VehicleID'))
-    Role = db.Column(db.String)
