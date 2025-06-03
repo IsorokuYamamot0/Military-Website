c5068963cf79dd23e980af44ba942c9cc9e8c658
IsorokuYamamot0 <21437@burnside.school.nz>
2025-06-04 11:19:02 +1200

    Bug fix on the models.py and the routes.py

--- app.py
+++ app.py
@@ -1,33 +1,7 @@
 from flask import Flask, render_template, request
-from models import db, Weapon
+from  models import db
 from app import app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///USA.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/search')
-def search():
-    query = request.args.get('q', '')
-    results = Weapon.query.filter(Weapon.name.ilike(f'%{query}%')).all()
-    return render_template('search_results.html', query=query, results=results)
-
-@app.route('/country/<name>')
-def country(name):
-    weapons = Weapon.query.filter_by(country=name).all()
-    return render_template('country.html', country=name, weapons=weapons)
-
-from models import Plane, Tank, ICBM, Helicopter, AirGroundMissile
-
-@app.route('/usa/planes')
-def usa_planes():
-    planes = Plane.query.all()
-    return render_template('planes.html', planes=planes)
 
 
 if __name__ == '__main__':
--- app/Project-13DTP/routes.py
+++ app/Project-13DTP/routes.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 import app.models as models
 import os
 
-
 app = Flask(__name__)
 
 
@@ -51,6 +50,32 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500_error.html'), 500
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///USA.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    query = request.args.get('q', '')
+    results = Weapon.query.filter(Weapon.name.ilike(f'%{query}%')).all()
+    return render_template('search_results.html', query=query, results=results)
+
+@app.route('/country/<name>')
+def country(name):
+    weapons = Weapon.query.filter_by(country=name).all()
+    return render_template('country.html', country=name, weapons=weapons)
+
+from models import Plane, Tank, ICBM, Helicopter, AirGroundMissile
+
+@app.route('/usa/planes')
+def usa_planes():
+    planes = Plane.query.all()
+    return render_template('planes.html', planes=planes)
 
 
 # Run the Flask application in debug mode if the script is executed directly
